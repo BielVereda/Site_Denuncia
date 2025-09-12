cfe80e55606d546188f6e3c009a79f030caaee82
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-09-12 11:10:10 -0300

    Delete src/assets directory

--- src/assets/scripts/pages/sobre.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const teamItems = document.querySelectorAll('.team-list li');
-    teamItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('highlight');
-        });
-    });
-});
